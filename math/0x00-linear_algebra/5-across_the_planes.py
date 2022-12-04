e9bfcf442d09605517ab4813ee9ee1048a0c124e
HassineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-12-04 23:33:37 +0100

    00-05

---
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""function returns matrices sum"""
+
+
+def add_matrices2D(mat1, mat2):
+    """matrix"""
+    if len(mat1[0]) != len(mat2[0]):
+        return None
+    res_matx = [[None for i in range(len(mat1))] for j in range(len(mat1[0]))]
+    for i in range(len(mat1[0])):
+        for j in range(len(mat1)):
+            res_matx[i][j] = mat1[i][j] + mat2[i][j]
+    return res_matx
