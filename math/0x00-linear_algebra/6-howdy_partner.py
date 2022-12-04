73ab9c32d2fff653b41d3e06deb5584510a57061
HassineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-12-04 23:47:42 +0100

    00-06fx

--- math/0x00-linear_algebra/6-howdy_partner.py
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -5,7 +5,8 @@
 def cat_arrays(arr1, arr2):
     """array"""
     res_arr = []
-    res_arr.append(arr1)
+    for i in range(len(arr1)):
+        res_arr.append(arr1[i])
     for i in range(len(arr2)):
         res_arr.append(arr2[i])
     return res_arr
