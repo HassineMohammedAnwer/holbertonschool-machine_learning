9f465232a202edb8043beca7d5b0b89fbe5326e5
HassineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-12-05 16:11:25 -0800

    0x01-04

--- math/0x01-plotting/4-frequency.py
+++ math/0x01-plotting/4-frequency.py
@@ -4,3 +4,13 @@ import matplotlib.pyplot as plt
 
 np.random.seed(5)
 student_grades = np.random.normal(68, 15, 50)
+
+
+plt.xlabel("Grades")
+plt.ylabel("Number of Students")
+plt.title("Project A")
+plt.axis([0, 100, 0, 30])
+plt.xlim(0, 100)
+plt.ylim(0, 30)
+plt.hist(student_grades,edgecolor='black',range=[0,100])
+plt.show()
