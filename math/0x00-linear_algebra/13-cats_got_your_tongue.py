2f7e2e4a6901ec7715605f8b42e84c7608e81b2b
HassineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-12-25 09:51:19 -0800

    00-13

---
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" func that imports numpy library """
+
+import numpy as np
+
+def np_cat(mat1, mat2, axis=0):
+    """func concatenates two matrices along a specific axis"""
+    return (np.concatenate((mat1, mat2), axis=axis))
