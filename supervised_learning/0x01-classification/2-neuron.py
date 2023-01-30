6bf888105496aef19d53fa6e1e00bbc740a694e2
HassineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2023-01-30 16:35:19 +0100

    02-07-08

---
+++ supervised_learning/0x01-classification/2-neuron.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Neuron"""
+
+
+import numpy as np
+
+
+class Neuron:
+    """defines a single neuron performing binary classification"""
+
+    def __init__(self, nx):
+        if not type(nx) is int:
+            raise TypeError('nx must be an integer')
+        elif (nx < 1):
+            raise ValueError('nx must be a positive integer')
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
