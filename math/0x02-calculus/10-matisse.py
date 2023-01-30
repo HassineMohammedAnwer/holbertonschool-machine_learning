b356af21e96e10d146acc9b9dc351049a4af6772
HassineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2023-01-30 18:17:55 +0100

    poly

---
+++ math/0x02-calculus/10-matisse.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""pythsdvsqdvqdsv"""
+
+
+def poly_derivative(poly):
+    """
+    polyezfaefaefaef
+    """
+    res = []
+    for i in range(1, len(poly)):
+        res.append(poly[i] * i)
+    return res
