7753b50c721a3d8c5c0a1c6647ab492e039ca4ad
HassineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2023-01-30 17:40:19 +0100

    kjukjh

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -2,9 +2,11 @@
 
 def summation_i_squared(n):
     """sum"""
-    if n != int and n <= 0:
+    if type(n) is not int:
         return None
-    if n <= 1:
-       return n
+    if n < 1:
+        return None
+    if n == 1:
+        return n
     else:
        return n * n + summation_i_squared(n-1)
