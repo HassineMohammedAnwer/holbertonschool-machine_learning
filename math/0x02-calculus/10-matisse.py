6004b719d470958090fe08d2b772d7e7679a0c3a
HassineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2023-01-30 18:20:50 +0100

    poly

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -8,6 +8,8 @@ def poly_derivative(poly):
     """
     if (type(poly) is not list) or poly == []:
         return None
+    if len(poly) < 2:
+        return [0]
     res = []
     for i in range(1, len(poly)):
         res.append(poly[i] * i)
