963665f98d8b13e2be60c337450ac9facc603335
HassineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2023-02-03 16:36:34 +0100

    03-02fix

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -32,3 +32,14 @@ class Poisson:
         for i in range(1, k + 1):
             fact = fact * i
         return ((Poisson.e**(-1 * self.lambtha) * lpowk) / fact)
+
+    def cdf(self, k):
+        """Calculates the value of the CDF for a given number of “successes”"""
+        if not isinstance(k, int):
+            k = int(k)
+        if k <= 0:
+            return 0
+        cdf = 0
+        for i in range(0, k + 1):
+            cdf += self.pmf(i)
+        return cdf
