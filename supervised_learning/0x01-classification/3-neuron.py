e69b6f337720b99bc5818e5333d15d5affe47040
HassineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2023-02-09 17:19:16 +0100

    01-03

---
+++ supervised_learning/0x01-classification/3-neuron.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""Neuron"""
+
+
+import numpy as np
+
+
+class Neuron:
+    """defines a single neuron performing binary classification"""
+
+    def __init__(self, nx):
+        if not type(nx) is int:
+            raise TypeError('nx must be an integer')
+        elif (nx < 1):
+            raise ValueError('nx must be a positive integer')
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
+
+    def forward_prop(self, X):
+        """Add Sigmoid Forward Prop Method"""
+        z = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-z))
+        return self.__A
+
+    def cost(self, Y, A):
+        """defines a single"""
+        m = Y.shape[1]
+        cost = - (1 / m) * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        return cost
