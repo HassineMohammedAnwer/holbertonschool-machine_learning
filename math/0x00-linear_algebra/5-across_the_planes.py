bbc7e7ac639b660324ae196172d724a251f4ee94
HassineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2023-02-17 18:23:35 +0100

    fxin

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -7,7 +7,8 @@ def add_matrices2D(mat1, mat2):
 
     if len(mat1) != len(mat2):
         return None
-
+    if any(len(row1) != len(row2) for row1, row2 in zip(mat1, mat2)):
+        return None
     result = []
     for i in range(len(mat1)):
         row = []
