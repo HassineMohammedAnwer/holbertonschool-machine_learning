dc2f2965b4361fe70141fc32312c367219f1e7bf
HassineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2023-02-17 18:17:47 +0100

    fxin

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -4,12 +4,15 @@
 
 def add_matrices2D(mat1, mat2):
     """matrix"""
-    if len(mat1[0]) != len(mat2[0]) :
+
+    if len(mat1) != len(mat2) or any(len(row1) != len(row2) for row1, row2 in zip(mat1, mat2)):
         return None
-    if len(mat1[0]) != len(mat2[0]):
-        return None
-    res_matx = [[None for i in range(len(mat1))] for j in range(len(mat1[0]))]
-    for i in range(len(mat1[0])):
-        for j in range(len(mat1)):
-            res_matx[i][j] = mat1[i][j] + mat2[i][j]
-    return res_matx
+
+    result = []
+    for i in range(len(mat1)):
+        row = []
+        for j in range(len(mat1[i])):
+            row.append(mat1[i][j] + mat2[i][j])
+        result.append(row)
+
+    return result
