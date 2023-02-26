e05143be19b768c3a970e98418db5f8e7b803f01
HassineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2023-02-26 17:45:52 +0100

    fx

---
+++ math/0x03-probability/binomial.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" class Binomial that represents a binomial distribution"""
+
+
+class Binomial:
+    """class cinomial"""
+    def __init__(self, data=None, n=1, p=0.5):
+        """Initialize binomial"""
+        if data is None:
+            if n <= 0:
+                raise ValueError("n must be a positive value")
+            if p <= 0 or p >= 1:
+                raise ValueError("p must be greater than 0 and less than 1")
+            self.n = int(n)
+            self.p = float(p)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            mean = sum(data) / len(data)
+            variance = sum((x - mean) ** 2 for x in data) / len(data)
+            self.n = int(round(mean ** 2 / (mean - variance)))
+            self.p = mean / self.n
+        self.n = int(self.n)
+        self.p = float(self.p)
