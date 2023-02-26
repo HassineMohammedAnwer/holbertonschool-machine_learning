21ec3c187f718426a72b9b8df8f452e5ff9cdd6b
HassineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2023-02-26 23:32:39 +0100

    fx

--- math/0x03-probability/binomial.py
+++ math/0x03-probability/binomial.py
@@ -24,3 +24,18 @@ class Binomial:
             self.p = mean / self.n
         self.n = int(self.n)
         self.p = float(self.p)
+
+    @staticmethod
+    def factorial(n):
+        """ function that calc the fact """
+        factorial_num = 1
+        for m in range(1, n + 1):
+            factorial_num *= m
+        return factorial_num
+
+    def pmf(self, k):
+        """Calculates the value of the PMF for a given number of successes"""
+        k = int(k)
+        if k < 0 or k > self.n:
+            return 0
+        return (self.factorial(self.n) / (self.factorial(k) * self.factorial(self.n - k))) * (self.p ** k) * ((1 - self.p) ** (self.n - k))
