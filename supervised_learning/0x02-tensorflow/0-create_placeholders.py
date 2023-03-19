c03bd4b64df131a319e6459ffdbeee10d10a8ec7
HassineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2023-03-19 18:40:49 +0100

    tensorflow-0

---
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""function that return two holders"""
+
+import tensorflow.compat.v1 as tf
+
+
+def create_placeholders(nx, classes):
+    """holders(x;y)"""
+    x = tf.placeholder(tf.float32, shape=[None, nx], name='x')
+    y = tf.placeholder(tf.float32, shape=[None, classes], name='y')
+    return x, y
