ab6b55a1828a6921aa5e337c6db8df6e402d3321
HassineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2023-03-28 18:48:31 +0200

    fxin

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -1,24 +1,15 @@
 #!/usr/bin/env python3
-"""The task is to implement a function called create_layer that creates
-  a new neural network layer. The function takes three arguments:
-**prev: the tensor output of the previous layer.
-**n: the number of nodes in the layer to create.
-**activation: the activation function that the layer should use.
-  The function should use the He et al. initialization for the layer weights,
-  which is implemented using
-  tf.keras.initializers.VarianceScaling(mode='fan_avg').
-  Each layer should be given the name layer.
-
-The function should return the tensor output of the layer"""
-
-import tensorflow as tf
+"""Defines `create_layer`."""
+import tensorflow.compat.v1 as tf
 
 
 def create_layer(prev, n, activation):
-    """create layer"""
-    pid = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    flake = tf.layers.Dense(units=n, activation=activation,
-                            kernel_initializer=deb, kernel_constraint=None,
-                            name='layer')
-
-    return flake(prev)
+    """ Creates a neural network layer  """
+    layer = tf.layers.Dense(
+        activation=activation,
+        name="layer",
+        units=n,
+        kernel_initializer=tf.keras.initializers.VarianceScaling(
+            mode='fan_avg')
+    )
+    return layer(prev)
