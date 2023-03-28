fd2e5364c47eb5958a1c79e42ba9401232aa0d5c
HassineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2023-03-29 01:09:26 +0200

    5-tnsrflw

---
+++ supervised_learning/0x02-tensorflow/5-create_train_op.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""qrgerqer"""
+
+import tensorflow.compat.v1 as tf
+
+def create_train_op(loss, alpha):
+    """
+    Creates the training operation for the network using gradient descent.
+
+    Args:
+        loss: the loss of the network's prediction.
+        alpha: the learning rate.
+
+    Returns:
+        An operation that trains the network using gradient descent.
+    """
+    op = tf.train.GradientDescentOptimizer(alpha)
+    train_op = op.minimize(loss)
+    return train_op
+
