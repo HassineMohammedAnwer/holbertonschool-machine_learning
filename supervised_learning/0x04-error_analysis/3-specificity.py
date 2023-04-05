9877f7f90c4ba1cf1b71b3bbfa0b470281ddb2a6
HassineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2023-04-06 01:00:21 +0200

    04-03

--- supervised_learning/0x04-error_analysis/3-specificity.py
+++ supervised_learning/0x04-error_analysis/3-specificity.py
@@ -7,7 +7,8 @@ import numpy as np
 def specificity(confusion):
     """The specificity is defined as the ratio of true negatives to
     the total number of actual negatives"""
-    tmp = np.sum(confusion, axis=1) + np.diag(confusion)
-    true_negatives = np.sum(confusion) - np.sum(confusion, axis=0) - tmp
+    tmp = np.diag(confusion)
+    tmp2 = np.sum(confusion, axis=1)
+    t_negatives = np.sum(confusion) - np.sum(confusion, axis=0) - tmp2 + tmp
     false_positives = np.sum(confusion, axis=0) - np.diag(confusion)
-    return true_negatives / (true_negatives + false_positives)
+    return t_negatives / (t_negatives + false_positives)
