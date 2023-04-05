175b31c0c5212537fd3c4b7e865284797cd782f2
HassineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2023-04-05 23:38:57 +0200

    04-00fx

--- supervised_learning/0x04-error_analysis/0-create_confusion.py
+++ supervised_learning/0x04-error_analysis/0-create_confusion.py
@@ -2,6 +2,7 @@
 """mmkjkj"""
 import numpy as np
 
+
 def create_confusion_matrix(labels, logits):
     """the function compares the predicted labels derived from
     the logits with the true labels, and updates the confusion
@@ -9,11 +10,11 @@ def create_confusion_matrix(labels, logits):
     for each pair of predicted and true labels."""
     m, classes = labels.shape
     confusion = np.zeros((classes, classes), dtype=np.int32)
-    
+
     predicted_labels = np.argmax(logits, axis=1)
     correct_labels = np.argmax(labels, axis=1)
-    
+
     for i in range(m):
         confusion[correct_labels[i], predicted_labels[i]] += 1
-        
+
     return confusion.astype(np.float64)
