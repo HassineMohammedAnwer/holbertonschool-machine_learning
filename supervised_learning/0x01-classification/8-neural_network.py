988d5cebb141a8deb1315f18dbabaa2bc64ef115
HassineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2023-06-14 13:42:27 +0100

    neural_netwrk

--- supervised_learning/0x01-classification/8-neural_network.py
+++ supervised_learning/0x01-classification/8-neural_network.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python3
-"""Defines NeuralNetwork"""
+"""Defines NeuralNetwork """
 import numpy as np
 
 
 class NeuralNetwork:
-    """A neural network."""
+    """NeuralNetwork class"""
 
     def __init__(self, nx, nodes):
-        """
-        Initializes a NeuralNetwork.
-
-        Args:
-            nx: The number of input features.
-            nodes: The number of nodes in the hidden layer.
-        """
-        if type(nx) is not int:
+        """NeuralNetwork class constructor"""
+        if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
@@ -22,6 +16,7 @@ class NeuralNetwork:
             raise TypeError("nodes must be an integer")
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
+
         self.W1 = np.random.randn(nodes, nx)
         self.b1 = np.zeros((nodes, 1))
         self.A1 = 0
