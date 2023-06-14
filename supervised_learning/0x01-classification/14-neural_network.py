5ab921230d353ba890bd5f3f3d6541e3f4144807
HassineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2023-06-14 15:24:14 +0100

    14-NN

---
+++ supervised_learning/0x01-classification/14-neural_network.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+"""Defines NeuralNetwork """
+import numpy as np
+
+
+class NeuralNetwork:
+    """NeuralNetwork class"""
+
+    def __init__(self, nx, nodes):
+        """NeuralNetwork class constructor"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        return self.__W1
+
+    @property
+    def b1(self):
+        return self.__b1
+
+    @property
+    def A1(self):
+        return self.__A1
+
+    @property
+    def W2(self):
+        return self.__W2
+
+    @property
+    def b2(self):
+        return self.__b2
+
+    @property
+    def A2(self):
+        return self.__A2
+
+    def forward_prop(self, X):
+        """result of the first layer's activation is stored in A1,
+        and the output layer's activation is stored in A2"""
+        Z1 = np.dot(self.__W1, X) + self.__b1
+        self.__A1 = 1 / (1 + np.exp(-Z1))
+
+        Z2 = np.dot(self.__W2, self.__A1) + self.__b2
+        self.__A2 = 1 / (1 + np.exp(-Z2))
+
+        return self.__A1, self.__A2
+
+    def cost(self, Y, A):
+        """calculates the cost of the model using logistic regression"""
+        m = Y.shape[1]
+        epsilon = 0.0000001
+        log_loss = -(Y * np.log(A) + (1 - Y) * np.log(epsilon + 1 - A))
+        cost = np.mean(log_loss)
+        return cost
+
+    def evaluate(self, X, Y):
+        """
+        evaluates the Neuron's predictions.Returns prediction and the cost
+        """
+        A = self.forward_prop(X)[1]
+        predictions = np.where(A >= 0.5, 1, 0)
+        cost = self.cost(Y, A)
+        return (predictions, cost)
+
+    def gradient_descent(self, X, Y, A1, A2, alpha=0.05):
+        """gradient_descent"""
+        m = X.shape[1]
+        dZ2 = A2 - Y
+        dW2 = np.matmul(dZ2, A1.T) / m
+        db2 = np.sum(dZ2, axis=1, keepdims=True) / m
+        dZ1 = np.matmul(self.__W2.T, dZ2) * (A1 * (1 - A1))
+        dW1 = np.matmul(dZ1, X.T) / m
+        db1 = np.sum(dZ1, axis=1, keepdims=True) / m
+        self.__W1 -= alpha * dW1
+        self.__b1 -= alpha * db1
+        self.__W2 -= alpha * dW2
+        self.__b2 -= alpha * db2
+
+    def train(self, X, Y, iterations=5000, alpha=0.05):
+        """ Train method code"""
+        if not isinstance(iterations, int):
+            raise TypeError("iterations must be an integer")
+        if iterations <= 0:
+            raise ValueError("iterations must be a positive integer")
+        if not isinstance(alpha, float):
+            raise TypeError("alpha must be a float")
+        if alpha <= 0:
+            raise ValueError("alpha must be positive")
+        
+        for i in range(iterations):
+            A1, A2 = self.forward_prop(X)
+            self.gradient_descent(X, Y, A1, A2, alpha)
+        
+        A, cost = self.evaluate(X, Y)
+        return A, cost
