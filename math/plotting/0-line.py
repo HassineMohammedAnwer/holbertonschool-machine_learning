b2b9d9cfe27220c19f4ca450774a52524c2ec90a
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-09-05 14:18:36 +0000

    math_plot_0

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -3,11 +3,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def line():
     """khm"""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
-    plt.plot(y,'r-')
-    plt.xlim(0,10)
+    plt.plot(y, 'r-')
+    plt.xlim(0, 10)
     plt.show()
