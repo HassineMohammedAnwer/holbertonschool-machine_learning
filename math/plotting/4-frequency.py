47a5897673cd7d49525618d8de7c5bd07bed9add
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-09-05 17:40:22 +0000

    math/plotting_1->6

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
+"""plot a histogram of student scores """
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-np.random.seed(5)
-student_grades = np.random.normal(68, 15, 50)
 
+def frequency():
+    """histogram"""
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
 
-plt.xlabel("Grades")
-plt.ylabel("Number of Students")
-plt.title("Project A")
-plt.axis([0, 100, 0, 30])
-plt.xlim(0, 100)
-plt.ylim(0, 30)
-plt.hist(student_grades,edgecolor='black',range=[0,100])
-plt.show()
+    plt.xlabel("Grades")
+    plt.ylabel("Number of Students")
+    plt.title("Project A")
+    plt.axis([0, 100, 0, 30])
+    plt.xlim(0, 100)
+    plt.ylim(0, 30)
+    plt.hist(student_grades,edgecolor='black',range=[0,100])
+    plt.show()
--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python3
+"""plot a stacked bar graph"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-np.random.seed(5)
-fruit = np.random.randint(0, 20, (4,3))
+
+def bars():
+    """bar graph: """
+    np.random.seed(5)
+    fruit = np.random.randint(0, 20, (4, 3))
+    plt.figure(figsize=(6.4, 4.8))
+
+    fig, ax = plt.subplots()
+    person = ["Farrah", "Fred", "Felicia"]
+    color = ['red', 'yellow', '#ff8000', '#ffe5b4']
+    fru = ['apples', 'bananas', 'oranges', 'peaches']
+    bottom = np.zeros(len(people))
+    for i, row in enumerate(fruit):
+        ax.bar(person, row, bottom=bottom,
+               color=color[i], label=fru[i], width=0.5)
+        bottom += row
+
+    ax.set_ylabel('Quantity of Fruit')
+    ax.set_title('Number of Fruit per Person')
+    ax.set_ylim([0, 80])
+    ax.set_yticks(np.arange(0, 80, 10))
+    ax.legend()
+    plt.show()
