e53003ed6a4fb7fb978e1eeccb7aefe904c9d8d4
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-09-15 14:24:10 +0200

    supervised_regularization/3

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -5,13 +5,11 @@ import tensorflow as tf
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """L2 reg """
-    l2_regularizer = tf.keras.regularizers.L2(lambtha)
+    layer_weight = tf.initializers.VarianceScaling(
+        scale=2.0, mode=("fan_avg"))
+    L2_regularization = tf.keras.regularizers.L2(lambtha)
 
-    layer = tf.keras.layers.Dense(
-        units=n,
-        activation=activation,
-        kernel_regularizer=l2_regularizer
-    )
-
-    # Apply the layer to the previous tensor (prev)
+    layer = tf.keras.layers.Dense(units=n, activation=activation,
+                                  kernel_initializer=layer_weight,
+                                  kernel_regularizer=L2_regularization)
     return layer(prev)
