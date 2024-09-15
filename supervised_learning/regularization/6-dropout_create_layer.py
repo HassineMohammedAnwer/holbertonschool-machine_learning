25e4dff55c736945f83ad1145dc17069be6c7e88
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-09-15 17:34:26 +0200

    supervised_regularization/6

--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -3,18 +3,20 @@
 import tensorflow as tf
 
 
-def dropout_create_layer(prev, n, activation, keep_prob):
+def dropout_create_layer(prev, n, activation, keep_prob,training=True):
     """prev tensor = output of the previous layer
     n number of nodes the new layer
     activation function that should be used on the layer
     keep_prob is the proba that a node will be kept
     Returns: the output of the new layer"""
-    kernel_init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    kernel_reg = tf.layers.Dropout(keep_prob)
-    layer = tf.layers.Dense(units=n,
-                            activation=activation,
-                            kernel_initializer=kernel_init,
-                            kernel_regularizer=kernel_reg,
-                            bias_regularizer=None,
-                            name='layer')
-    return layer(prev)
+    initializer = tf.keras.initializers.VarianceScaling(
+        scale=2.0, mode='fan_avg')
+    layer = tf.keras.layers.Dense(
+        units=n,
+        activation=activation,
+        kernel_initializer=initializer)(prev)
+
+    if training:
+        dropout = tf.nn.dropout(layer, rate=1 - keep_prob)
+
+    return dropout
