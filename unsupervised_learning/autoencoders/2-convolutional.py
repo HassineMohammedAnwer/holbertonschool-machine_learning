737b4ba85032e29951c5f43f63a424bcd1bc9861
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-09-24 11:40:49 +0200

    encoder_conv_and_vae

--- unsupervised_learning/autoencoders/2-convolutional.py
+++ unsupervised_learning/autoencoders/2-convolutional.py
@@ -18,4 +18,55 @@ def autoencoder(input_dims, filters, latent_dims):
             decoder: decoder model
             auto: the full autoencoder model
     """
+    x = keras.Input(shape=input_dims)
 
+    encoder_conv = keras.layers.Conv2D(filters=filters[0], kernel_size=(3, 3),
+                                       padding='same',
+                                       activation='relu')(x)
+
+    encoder_pool = keras.layers.MaxPooling2D(pool_size=(2, 2),
+                                             padding="same")(encoder_conv)
+
+    for i in range(1, len(filters)):
+        encoder_conv = keras.layers.Conv2D(filters=filters[i],
+                                           kernel_size=(3, 3), padding='same',
+                                           activation='relu')(encoder_pool)
+        encoder_pool = keras.layers.MaxPooling2D(pool_size=(2, 2),
+                                                 padding="same")(encoder_conv)
+
+    latent_ly = encoder_pool
+    encoder = keras.Model(x, latent_ly)
+
+    X_decoder = keras.Input(shape=latent_dims)
+    decoder_conv = keras.layers.Conv2D(filters=filters[-1],
+                                       kernel_size=(3, 3),
+                                       padding='same',
+                                       activation='relu')(X_decoder)
+
+    decoder_pool = keras.layers.UpSampling2D((2, 2))(decoder_conv)
+
+    for j in range(len(filters) - 2, 0, -1):
+        decoder_conv = keras.layers.Conv2D(filters=filters[j],
+                                           kernel_size=(3, 3),
+                                           padding='same',
+                                           activation='relu')(decoder_pool)
+        decoder_pool = keras.layers.UpSampling2D((2, 2))(decoder_conv)
+
+    decoder_conv = keras.layers.Conv2D(filters=filters[0], kernel_size=(3, 3),
+                                       padding='valid',
+                                       activation='relu')(decoder_pool)
+
+    decoder_pool = keras.layers.UpSampling2D((2, 2))(decoder_conv)
+
+    output = keras.layers.Conv2D(filters=input_dims[-1], kernel_size=(3, 3),
+                                 padding='same',
+                                 activation='sigmoid')(decoder_pool)
+
+    decoder = keras.Model(X_decoder, output)
+
+    X_input = keras.Input(shape=input_dims)
+    e_output = encoder(X_input)
+    d_output = decoder(e_output)
+    auto = keras.Model(inputs=X_input, outputs=d_output)
+    auto.compile(loss='binary_crossentropy', optimizer='adam')
+    return encoder, decoder, auto
