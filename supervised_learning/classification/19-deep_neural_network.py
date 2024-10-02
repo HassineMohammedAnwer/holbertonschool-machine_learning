e3d270a9a75d32217d4526ae7fde565caf10e4a1
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-10-02 22:51:17 +0200

    supervised_classification/19-deep_neural_network_update

--- supervised_learning/classification/19-deep_neural_network.py
+++ supervised_learning/classification/19-deep_neural_network.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Defines deep NeuralNetwork """
+"""Defines deep NeuralNetwork """
 import numpy as np
 
 
@@ -10,21 +10,23 @@ class DeepNeuralNetwork:
         """DeepNeuralNetwork class constructor"""
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
-
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-
         if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
 
         self.__L = len(layers)
         self.__cache = {}
-        self.__weights = {
-            'W1': np.random.randn(layers[0], nx) * np.sqrt(2 / nx),
-            'b1': np.zeros((layers[0], 1))
-        }
+        self.__weights = {'W1':
+                          np.random.randn(layers[0], nx) * np.sqrt(2 / nx),
+                          'b1': np.zeros((layers[0], 1))
+                          }
+        if not isinstance(layers[0], int) or layers[0] <= 0:
+            raise TypeError("layers must be a list of positive integers")
 
-        for i in range(1, self.L):
+        for i in range(1, self.__L):
+            if not isinstance(layers[i], int) or layers[i] <= 0:
+                raise TypeError("layers must be a list of positive integers")
             self.__weights["W" + str(i + 1)] =\
                 np.random.randn(layers[i], layers[i - 1]) *\
                 np.sqrt(2 / layers[i - 1])
@@ -32,7 +34,7 @@ class DeepNeuralNetwork:
 
     @property
     def L(self):
-        """  number of layers in the neural network"""
+        """ number of layers in the neural network"""
         return self.__L
 
     @property
