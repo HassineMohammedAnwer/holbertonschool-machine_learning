1738789696c2a82609b30d84c2bf63995381f4b6
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-10-02 20:47:02 +0200

    supervised_classification/16-deep_neural_network

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -10,11 +10,11 @@ class DeepNeuralNetwork:
         """DeepNeuralNetwork class constructor"""
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
-
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-
         if not isinstance(layers, list) or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+        if not all(isinstance(l, int) and l > 0 for l in layers):
             raise TypeError("layers must be a list of positive integers")
 
         self.L = len(layers)
