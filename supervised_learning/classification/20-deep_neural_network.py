5ecc7f5ce53297383dfb3d587e0a95b5f7f25143
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-10-02 23:20:18 +0200

    supervised_classification/20-deep_neural_network_update

--- supervised_learning/classification/20-deep_neural_network.py
+++ supervised_learning/classification/20-deep_neural_network.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Defines deep NeuralNetwork """
+"""Defines deep NeuralNetwork """
 import numpy as np
 
 
@@ -10,21 +10,23 @@ class DeepNeuralNetwork:
         """DeepNeuralNetwork class constructor"""
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
-
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-
         if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
 
         self.__L = len(layers)
         self.__cache = {}
-        self.__weights = {
-            'W1': np.random.randn(layers[0], nx) * np.sqrt(2 / nx),
-            'b1': np.zeros((layers[0], 1))
-        }
+        self.__weights = {'W1':
+                          np.random.randn(layers[0], nx) * np.sqrt(2 / nx),
+                          'b1': np.zeros((layers[0], 1))
+                          }
+        if not isinstance(layers[0], int) or layers[0] <= 0:
+            raise TypeError("layers must be a list of positive integers")
 
-        for i in range(1, self.L):
+        for i in range(1, self.__L):
+            if not isinstance(layers[i], int) or layers[i] <= 0:
+                raise TypeError("layers must be a list of positive integers")
             self.__weights["W" + str(i + 1)] =\
                 np.random.randn(layers[i], layers[i - 1]) *\
                 np.sqrt(2 / layers[i - 1])
@@ -32,7 +34,7 @@ class DeepNeuralNetwork:
 
     @property
     def L(self):
-        """  number of layers in the neural network"""
+        """ number of layers in the neural network"""
         return self.__L
 
     @property
@@ -63,17 +65,17 @@ class DeepNeuralNetwork:
 
     def evaluate(self, X, Y):
         """Return: Returns the neuron’s prediction and the cost
-           prediction: a numpy.ndarray with shape (1, m) of predicted labels for each example
-           predicted labels for each example and the label values should be 1
-           The label values should be 1 if the output of the network is >= 0.5 and 0 otherwise
+           prediction: a numpy.ndarray with shape (1, m) of predicted labels
+           for each example predicted labels for each example and the label
+           values should be 1
+           The label values should be 1 if the output of the network is >= 0.5
+           and 0 otherwise
         """
         A, _ = self.forward_prop(X)  # Perform forward propagation
 
         # Compute the predictions (1 if A >= 0.5, 0 otherwise)
         predictions = np.where(A >= 0.5, 1, 0)
 
-        # Calculate the cost using binary cross-entropy loss
-        m = Y.shape[1]
-        cost = -np.sum(Y * np.log(A) + (1 - Y) * np.log(1 - A)) / m
-
+        # Calculate the cost
+        cost = self.cost(Y, A)
         return predictions, cost
