e1d77d9e56ec86b781b083576054ade1195890d3
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-10-08 08:09:46 +0200

    supervised/tensorflow/6-train_update

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -8,7 +8,8 @@ create_train_op = __import__('5-create_train_op').create_train_op
 forward_prop = __import__('2-forward_prop').forward_prop
 
 
-def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, iterations, save_path="/tmp/model.ckpt"):
+def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha,
+          iterations, save_path="/tmp/model.ckpt"):
     """ builds, trains, and saves a neural network classifier"""
     m, nx = X_train.shape
     classes = Y_train.shape[1]
@@ -32,9 +33,11 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, i
         for i in range(iterations + 1):
             sess.run(train_op, feed_dict={x: X_train, y: Y_train})
             if i == 0 or i % 100 == 0:
-                train_loss, train_acc = sess.run([loss, accuracy], feed_dict={x: X_train, y: Y_train})
+                train_loss, train_acc = sess.run(
+                    [loss, accuracy], feed_dict={x: X_train, y: Y_train})
 
-                valid_loss, valid_acc = sess.run([loss, accuracy], feed_dict={x: X_valid, y: Y_valid})
+                valid_loss, valid_acc = sess.run(
+                    [loss, accuracy], feed_dict={x: X_valid, y: Y_valid})
 
                 print(f"After {i} iterations:")
                 print(f"\tTraining Cost: {train_loss}")
