37af6fb18f6c4b7d4d4a17fd5cefa0f5ee786033
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-10-08 08:20:33 +0200

    supervised/tensorflow/6-train_update

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -23,13 +23,14 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha,
     tf.add_to_collection('x', x)
     tf.add_to_collection('y', y)
     tf.add_to_collection('y_pred', y_pred)
-    tf.add_to_collection('loss', loss)
     tf.add_to_collection('accuracy', accuracy)
+    tf.add_to_collection('loss', loss)
     tf.add_to_collection('train_op', train_op)
 
     saver = tf.train.Saver()
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
+
         for i in range(iterations + 1):
             sess.run(train_op, feed_dict={x: X_train, y: Y_train})
             train_loss, train_acc = sess.run(
@@ -43,6 +44,9 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha,
                 print(f"\tTraining Accuracy: {train_acc}")
                 print(f"\tValidation Cost: {valid_loss}")
                 print(f"\tValidation Accuracy: {valid_acc}")
+
+            if i < iterations:
+                sess.run(train_op, feed_dict={x: X_train, y: Y_train})
 
         save_path = saver.save(sess, save_path)
 
