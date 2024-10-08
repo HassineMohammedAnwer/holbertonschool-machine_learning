af3c5e5f76c0b4e8552b91b0f34ee4224a630068
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-10-08 08:04:44 +0200

    supervised/tensorflow/6-train

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -5,7 +5,7 @@ import tensorflow.compat.v1 as tf
 
 def create_layer(prev, n, activation):
     """ Creates a neural network layer  """
-    layer = tf.layers.Dense(
+    layer = tf.keras.layers.Dense(
         activation=activation,
         name="layer",
         units=n,
