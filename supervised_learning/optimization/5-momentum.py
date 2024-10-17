ff3c35b2a7acf7c834b9166fdb0f18e44e5d980d
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-10-17 17:39:27 +0200

    math/optimization/05

---
+++ supervised_learning/optimization/5-momentum.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""5. Momentum"""
+
+
+def update_variables_momentum(alpha, beta1, var, grad, v):
+    """
+         updates variable using gradient descent with momentum optimization algo
+    """
+    var_new = beta1 * v + (1 - beta1) * grad
+    new_var = var - var_new * alpha
+    return new_var, var_new
