629e8153475c9046e2d5a5a5960e401aeae15b4c
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-10-17 22:52:27 +0200

    math/optimization/06_update_style

--- supervised_learning/optimization/6-momentum.py
+++ supervised_learning/optimization/6-momentum.py
@@ -4,10 +4,8 @@ import tensorflow as tf
 
 
 def create_momentum_op(alpha, beta1):
-    """sets up the gradient descent with momentum optimization algorithm in TensorFlow
-    """
-    """optimizer = tf.train.MomentumOptimizer(learning_rate=alpha,
-                                           momentum=beta1)"""
+    """sets up the gradient descent with momentum
+    optimization algorithm in TensorFlow"""
     optimizer = tf.keras.optimizers.SGD(learning_rate=alpha,
                                         momentum=beta1)
     return optimizer
