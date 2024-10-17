f78dcafe2108a869af4bca7324593e9dcecf7748
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-10-17 23:39:54 +0200

    math/optimization/08_update_beta2

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -7,5 +7,5 @@ def create_RMSProp_op(alpha, beta2, epsilon):
     """sets up the RMSProp optimization
     algorithm in TensorFlow"""
     optimizer = tf.keras.optimizers.RMSprop(learning_rate=alpha,
-                                            decay=beta2, epsilon=epsilon)
+                                            rho=beta2, epsilon=epsilon)
     return optimizer
