8a29fe2bbbe68df750e1591b531123e117e6f69e
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-10-17 16:12:45 +0200

    math/multivariate_prob/03

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""2. Initialize"""
+"""3. PDF"""
 import numpy as np
 
 
@@ -15,3 +15,29 @@ class MultiNormal:
         self.mean = np.mean(data, axis=1, keepdims=True)
         data_centered = data - self.mean
         self.cov = np.dot(data_centered, data_centered.T) / (n - 1)
+
+    def pdf(self, x):
+        """Probability Density Function
+        f(x) = (1 / (sqrt((2 * π)^d * det(Σ)))) *
+              exp(-0.5 * (x - μ)^T * Σ^(-1) * (x - μ))
+        
+        d is the number of dimensions (i.e., the size of the mean vector μ)
+        Σ is the covariance matrix of the distribution
+        det(Σ) determinant of the covariance matrix
+        Σ^(-1) is the inverse of the covariance matrix
+
+        """
+        if not isinstance(x, np.ndarray):
+            raise TypeError("x must be a numpy.ndarray")
+        d = self.mean.shape[0]
+        if x.shape != (d, 1):
+            raise ValueError("x must have the shape ({}, 1)".
+                             format(d))
+        tmp = x - self.mean
+        cov_det = np.linalg.det(self.cov)
+        cov_inv = np.linalg.inv(self.cov)
+        tmp2 = np.dot(tmp.T, cov_inv)
+        exponent = -0.5 * np.dot(tmp2, tmp)
+        coefficient = (1 / np.sqrt((2 * np.pi) ** d * cov_det))
+        pdf_value = coefficient * np.exp(exponent)
+        return pdf_value[0][0]
