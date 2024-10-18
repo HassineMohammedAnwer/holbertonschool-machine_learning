c485bf5c6d411e6d5928242534aee972d6ad7369
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-10-18 16:27:01 +0200

    math/optimization/09_update_style+/10_update_style/+/11

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -7,7 +7,7 @@ def create_Adam_op(alpha, beta1, beta2, epsilon):
     """sets up the Adam optimization
     algorithm in TensorFlow"""
     optimizer = tf.keras.optimizers.Adam(learning_rate=alpha,
-                                        beta_1=beta1,
-                                        beta_2=beta2,
-                                        epsilon=epsilon)
+                                         beta_1=beta1,
+                                         beta_2=beta2,
+                                         epsilon=epsilon)
     return optimizer
---
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""11. Learning Rate Decay"""
+
+
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """updates learning rate using inverse time decay in numpy"""
+    return alpha / (1 + decay_rate * (global_step / decay_step))
