17b7eff1fd088063802770ab637e34096952018b
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-10-19 22:51:37 +0200

    supervised_learning/keras/04_update/05_train

--- supervised_learning/keras/5-train.py
+++ supervised_learning/keras/5-train.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-"""4. Train"""
+"""5. Validate"""
 import tensorflow.keras as K
 
 
 def train_model(network, data, labels, batch_size,
-                epochs, verbose=True, shuffle=False):
-    """trains a model using mini-batch gradient descent:
+                epochs, validation_data=None, verbose=True, shuffle=False):
+    """
+    trains model using mini-bch gradient descent_-and_-analyzes validaiton data
     network is the model to train
     data is a numpy.ndarray of shape (m, nx) containing the input data
     labels one-hot numpy.ndarray of shape(m, classes)containing labels of data
@@ -14,12 +15,14 @@ def train_model(network, data, labels, batch_size,
     verbose boolean determines if output should be printed during training
     shuffle boolean determines whether to shuffle batches every epoch.Normally,
       it's a good idea to shuffle,but for reproducibility,we set default=False.
+    validation_data is the data to validate the model with, if not None
     Returns: the History object generated after training the model"""
     History = network.fit(
         data,
         labels,
         batch_size=batch_size,
         verbose=verbose,
+        validation_data=validation_data,
         epochs=epochs,
         shuffle=shuffle)
     return History
