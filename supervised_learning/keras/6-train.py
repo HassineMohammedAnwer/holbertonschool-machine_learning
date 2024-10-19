7aae141b385b1693ec661c6ff3a3948e2f346385
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-10-19 23:10:25 +0200

    supervised_learning/keras/06_early_stopping

---
+++ supervised_learning/keras/6-train.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""6. Early Stopping"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False, patience=0,
+                verbose=True, shuffle=False):
+    """
+    trains model using mini-bch gradient descent and analyzes validation data
+      also trains the model using early stopping
+    network is the model to train
+    data is a numpy.ndarray of shape (m, nx) containing the input data
+    labels one-hot numpy.ndarray of shape(m, classes)containing labels of data
+    batch_size is the size of the batch used for mini-batch gradient descent
+    epochs is the number of passes through data for mini-batch gradient descent
+    verbose boolean determines if output should be printed during training
+    shuffle boolean determines whether to shuffle batches every epoch.Normally,
+      it's a good idea to shuffle,but for reproducibility,we set default=False.
+    validation_data is the data to validate the model with, if not None
+    early_stopping boolean that indicates whether early stopping should be used
+      early stopping should only be performed if validation_data exists
+      early stopping should be based on validation loss
+    patience is the patience used for early stopping
+    Returns: the History object generated after training the model"""
+    if validation_data:
+        callback = K.callbacks.EarlyStopping(
+            monitor='val_loss',
+            patience=patience,
+            verbose=verbose,
+        )
+    else:
+        callback = None
+    History = network.fit(
+        data,
+        labels,
+        batch_size=batch_size,
+        verbose=verbose,
+        callbacks=callback,
+        validation_data=validation_data,
+        epochs=epochs,
+        shuffle=shuffle)
+    return History
