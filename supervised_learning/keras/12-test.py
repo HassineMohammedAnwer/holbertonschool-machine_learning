1ed122d71f3b7ca25478453c01bc482d5f69b76c
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-10-20 20:19:11 +0200

    supervised_learning/keras/12_update_style

--- supervised_learning/keras/12-test.py
+++ supervised_learning/keras/12-test.py
@@ -8,7 +8,8 @@ def test_model(network, data, labels, verbose=True):
     network is the network model to test
     data is the input data to test the model with
     labels are the correct one-hot labels of data
-    verbose boolean determines if output should be printed during testing process
+    verbose boolean determines if output should be printed
+      during testing process
     Returns: loss and accuracy of model with testing data,respectively"""
     return network.evaluate(x=data,
                             y=labels,
---
+++ supervised_learning/keras/13-predict.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""13. Predict"""
+import tensorflow.keras as K
+
+
+def predict(network, data, verbose=False):
+    """ that makes a prediction using a neural network:
+    network is the network model to make the prediction with
+    data is the input data to make the prediction with
+    verbose is a boolean that determines if output should be
+      printed during the prediction process
+    Returns: the prediction for the data"""
+    return 
