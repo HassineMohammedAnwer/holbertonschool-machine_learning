98248543a6fe788a24f37a0c6e3d728790782541
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-10-21 13:29:01 +0200

    math/1

---
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""1. Same Convolution"""
+import numpy as np
+
+
+def convolve_grayscale_same(images, kernel):
+    """flmefvlù"""
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+    # output_h = h AND output_w = w
+    output = np.zeros((m, h, w))
+    padding_for_height = kh // 2
+    padding_for_weight = kw // 2
+    padded_images = np.pad(images, pad_width=((0,),
+        (padding_for_height,), (padding_for_weight,)))
+
+    for i in range(h):
+        for j in range(w):
+            output[:, i, j] = np.sum(padded_images[:, i:i+kh, j:j+kw] * kernel,
+                                     axis=(1, 2))
+
+    return output
