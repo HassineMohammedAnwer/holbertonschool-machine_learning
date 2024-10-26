88f519ed3e1adbe9545f45202e2d14b544866580
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-10-26 19:03:06 +0200

    math/conv_and_pool/2

---
+++ math/convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""2. Convolution with Padding"""
+import numpy as np
+
+
+def convolve_grayscale_padding(images, kernel, padding):
+    """performs a convolution on grayscale images with custom padding
+    images is a numpy.ndarray (m, h, w) containing multiple grayscale images
+    m number of images
+    h height in pixels of the images
+    w width in pixels of the images
+    kernel numpy.ndarray (kh, kw) containing kernel for the convolution
+    kh is the height of the kernel
+    kw is the width of the kernel
+    padding is a tuple of (ph, pw)
+    ph is the padding for the height of the image
+    pw is the padding for the width of the image
+    the image should be padded with 0’s
+    You are only allowed to use two for loops; any other loops of any kind are not allowed
+    Returns: a numpy.ndarray containing the convolved images"""
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+    ph, pw = padding
+    output_h = h - kh + ph * 2 + 1
+    output_w = w - kw  + pw * 2 + 1
+    output = np.zeros((m, output_h, output_w))
+    padded_images = np.pad(images,
+                           pad_width=((0,),
+                                      (ph,),
+                                      (pw,)))
+
+    for i in range(h):
+        for j in range(w):
+            output[:, i, j] = np.sum(padded_images[:, i:i+kh, j:j+kw] * kernel,
+                                     axis=(1, 2))
+
+    return output
