2d16f2871a6c515e5d2d44fb8c81c87623f07ac6
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-10-28 06:59:40 +0100

    supervised/CNN/1

--- supervised_learning/cnn/1-pool_forward.py
+++ supervised_learning/cnn/1-pool_forward.py
@@ -1,38 +1,41 @@
 #!/usr/bin/env python3
-""" function thatConvol Forward Prop """
+"""0. Convolutional Forward Prop"""
 import numpy as np
 
 
 def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
-    """
-    forward propagation over a pooling layer
-    - A_prev: shape (m, h_prev, w_prev, c_prev):utput of previous layer.
-    - kernel_shape: tuple of (kh, kw): size of pooling kernel.
-    - stride: a tuple of (sh, sw): strides for pooling operation.
-    - mode: string: type of pooling,'max' or 'avg'.
-    - The output of pooling layer.
+    """performs forward propagation over a pooling layer of a nn:
+    A_prev numpy.ndarray(m, h_prev, w_prev, c_prev): output previous layer
+    m is the number of examples
+    h_prev is the height of the previous layer
+    w_prev is the width of the previous layer
+    c_prev is the number of channels in the previous layer
+    kernel_shape tuple(kh, kw): the size of the kernel for the pooling
+    kh is the kernel height
+    kw is the kernel width
+    stride is a tuple of (sh, sw) containing the strides for the pooling
+    sh is the stride for the height
+    sw is the stride for the width
+    mode is a string containing either max or avg,
+    you may import numpy as np
+    Returns: the output of the pooling layer
     """
     m, h_prev, w_prev, c_prev = A_prev.shape
     kh, kw = kernel_shape
     sh, sw = stride
-
-    # Calculate dimensions of the output feature map
-    out_h = int((h_prev - kh) / sh + 1)
-    out_w = int((w_prev - kw) / sw + 1)
-
-    # Initialize the result array to store the output feature map
-    result = np.zeros((m, out_h, out_w, c_prev))
-
-    for i in range(out_h):
-        for j in range(out_w):
-            for k in range(c_prev):
-                # Extract a slice of the input feature map
-                input_slice = A_prev[:, i * sh:i * sh + kh, j * sw:j * sw + kw, k]
-
-                # Apply pooling based on the mode
-                if mode == 'max':
-                    result[:, i, j, k] = np.max(input_slice, axis=(1, 2))
-                elif mode == 'avg':
-                    result[:, i, j, k] = np.mean(input_slice, axis=(1, 2))
-
-    return result
+    output_h = (h_prev - kh) // sh + 1
+    output_w = (w_prev - kw) // sw + 1
+    output = np.zeros((m, output_h, output_w, c_prev))
+    for i in range(output_h):
+        for j in range(output_w):
+            if mode == "max":
+                output[:, i, j, :] = np.max(
+                    A_prev[:, i * sh:i * sh + kh, j * sw:j * sw + kw],
+                    axis=(1, 2)
+                )
+            elif mode == 'avg':
+                output[:, i, j, :] = np.average(
+                    A_prev[:, i * sh:i * sh + kh, j * sw:j * sw + kw],
+                    axis=(1, 2)
+                )
+    return output
