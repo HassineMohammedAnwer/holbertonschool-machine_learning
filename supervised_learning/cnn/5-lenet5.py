fe6e3c745a4618d5cb091eb0a411dbfb97b321cd
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-10-29 03:54:03 +0100

    supervised/CNN/5_update_he_init

--- supervised_learning/cnn/5-lenet5.py
+++ supervised_learning/cnn/5-lenet5.py
@@ -24,26 +24,26 @@ def lenet5(x):
     Returns: a K.Model compiled to use Adam optimization
     __(with default hyperparameters) and accuracy metrics
     """
-    initializer = K.initializers.HeNormal()
+    # initializer = K.initializers.HeNormal()
     model = K.Sequential([
         K.layers.Conv2D(filters=6,
                         kernel_size=5,
                         padding='same',
-                        kernel_initializer=initializer,
+                        kernel_initializer='he_normal',
                         activation='relu'),
         K.layers.MaxPooling2D(pool_size=2,
                               strides=2),
         K.layers.Conv2D(filters=16,
                         kernel_size=5,
                         padding='valid',
-                        kernel_initializer=initializer,
+                        kernel_initializer='he_normal',
                         activation='relu'),
         K.layers.MaxPooling2D(pool_size=2,
                               strides=2),
         K.layers.Flatten(),
-        K.layers.Dense(120, activation='relu', kernel_initializer=initializer),
-        K.layers.Dense(84, activation='relu', kernel_initializer=initializer),
-        K.layers.Dense(10, activation='softmax', kernel_initializer=initializer)
+        K.layers.Dense(120, activation='relu', kernel_initializer='he_normal'),
+        K.layers.Dense(84, activation='relu', kernel_initializer='he_normal'),
+        K.layers.Dense(10, activation='softmax', kernel_initializer='he_normal')
     ])
     model.compile(loss='categorical_crossentropy',
                   optimizer=K.optimizers.Adam(),
