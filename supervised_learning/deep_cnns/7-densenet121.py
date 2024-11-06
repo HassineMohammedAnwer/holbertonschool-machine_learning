f404412314f472715e361fccb83ca3a757f9fc3c
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-11-06 02:03:04 +0100

    deep_cnns/7_update

--- supervised_learning/deep_cnns/7-densenet121.py
+++ supervised_learning/deep_cnns/7-densenet121.py
@@ -15,9 +15,6 @@ def densenet121(growth_rate=32, compression=1.0):
     __a rectified linear activation (ReLU), respectively
     All weights should use he normal initialization
     The seed for the he_normal initializer should be set to zero
-    You may use:
-    dense_block = __import__('5-dense_block').dense_block
-    transition_layer = __import__('6-transition_layer').transition_layer
     Returns: the keras model"""
     initializer = K.initializers.he_normal(seed=0)
     input_1 = K.Input(shape=(224, 224, 3))
