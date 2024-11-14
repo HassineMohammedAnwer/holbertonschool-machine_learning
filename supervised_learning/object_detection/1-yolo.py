0276872391b30471c41ccf2d00b28b45e5d4af3f
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-11-14 04:39:25 +0100

    yolo

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -32,6 +32,7 @@ class Yolo:
         self.class_t = class_t
         self.nms_t = nms_t
         self.anchors = anchors
+
         def process_outputs(self, outputs, image_size):
             """outputs is a list of numpy.ndarrays containing the predictions
             __from the Darknet model for a single image:
