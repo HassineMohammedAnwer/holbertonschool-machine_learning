e07f3e28cc9c084f534e675c92b8d967cc08f1b8
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-11-18 16:24:07 +0100

    pipeline/pandas/00_update_style

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -9,7 +9,8 @@ def from_numpy(array):
     The columns of the pd.DataFrame should be labeled in alphabetical order
     __and capitalized. There will not be more than 26 columns.
     Returns: the newly created pd.DataFrame"""
-    col = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-    num_col = array.size
-    df = pd.DataFrame(array, columns=col[:num_col])
-    return df
+    if array:
+        col = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        num_col = array.shape[1]
+        df = pd.DataFrame(array, columns=col[:num_col])
+        return df
