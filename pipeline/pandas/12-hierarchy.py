608a04ce83145de0bc7964c50d9e221a27e4a3a0
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-11-18 20:28:35 +0100

    pipeline/pandas/12_update

--- pipeline/pandas/12-hierarchy.py
+++ pipeline/pandas/12-hierarchy.py
@@ -22,4 +22,4 @@ def hierarchy(df1, df2):
     df2 = index(df2)
     df = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
     df = df.reorder_levels([1, 0], axis=0)
-    return df
+    return df.sort_index()
