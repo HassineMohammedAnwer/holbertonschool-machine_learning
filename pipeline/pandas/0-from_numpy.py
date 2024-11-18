f3a2cf5612fb71917fd14d455416c01f34628211
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-11-18 16:11:04 +0100

    pipeline/pandas/00

---
+++ pipeline/pandas/0-from_numpy.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import pandas as pd
+
+
+def from_numpy(array):
+    """creates a pd.DataFrame from a np.ndarray
+    array is the np.ndarray from which you should create the pd.DataFrame
+    The columns of the pd.DataFrame should be labeled in alphabetical order
+    __and capitalized. There will not be more than 26 columns.
+    Returns: the newly created pd.DataFrame"""
+    col = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    num_col = array.shape[1]
+    df = pd.DataFrame(array, columns=col[:num_col])
+    return df
