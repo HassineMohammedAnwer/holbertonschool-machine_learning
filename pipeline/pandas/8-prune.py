efbc37dff9c6886abac1a6a09b8ffe3ee6eed99c
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-11-18 19:44:01 +0100

    pipeline/pandas/08

---
+++ pipeline/pandas/8-prune.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""8. Prune"""
+
+
+def prune(df):
+    """takes a pd.DataFrame and:
+    Removes any entries where Close has NaN values.
+    Returns: the modified pd.DataFrame"""
+    return df.dropna(subset=["Close"])
