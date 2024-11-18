65db56da95b11939b021e31d58c36ee618376fe6
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-11-18 22:15:44 +0100

    api/00

---
+++ pipeline/apis/0-passengers.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""0. Can I join?"""
+import requests
+
+
+def availableShips(passengerCount):
+    """returns the list of ships that can hold a given number of passengers:
+    Prototype: def availableShips(passengerCount):
+    Don’t forget the pagination
+    If no ship available, return an empty list."""
+    url = 'https://swapi-api.hbtn.io/api/starships/'
+    req = requests.get(url).json()
+    starships = []
+
+    # for all pages
+    while req.get("next"):
+        starship = req.get("results")
+        for ship in starship:
+            passenger = ship.get("passengers")
+
+            if passenger == "n/a" or passenger == "unknown":
+                continue
+
+            if int(passenger.replace(",", "")) >= passengerCount:
+                starships.append(ship.get("name"))
+
+        req = requests.get(req.get("next")).json()
+
+    return starships
