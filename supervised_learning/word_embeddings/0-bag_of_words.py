ec25ea6bf8d7256a477d5dc2d870b1b71074885e
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-11-21 11:48:45 +0100

    NLP/00_update

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -26,7 +26,7 @@ def bag_of_words(sentences, vocab=None):
     # print('sentences_l')
     # print(sentences_l)
     if vocab is None:
-        vocab = sorted({word for sentence in sentences_l for word in sentence})
+        vocab = np.array(sorted({word for sentence in sentences_l for word in sentence}))
     num_sentences = len(sentences_l)
     num_features = len(vocab)
     embedding_matrix = np.zeros((num_sentences, num_features), dtype=int)
