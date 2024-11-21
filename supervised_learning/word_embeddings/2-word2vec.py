1737732baf2a9bf5530b64cbba5ac867e0636531
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-11-21 12:31:28 +0100

    NLP/02_update

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -15,7 +15,10 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5, negative=5
     seed is the seed for the random number generator
     workers is the number of worker threads to train the model
     Returns: the trained model"""
-    model = gensim.models.Word2Vec(sentences=sentences, vector_size=vector_size, window=window, min_count=min_count, workers=workers)
-    model.train(sentences, total_examples=model.corpus_count,
-                epochs=model.epochs)
+    #skip-gram and CBOW models, using either hierarchical softmax or negative sampling
+    # sg ({0, 1}, optional) – Training algorithm: 1 for skip-gram; otherwise CBOW.
+    model = gensim.models.Word2Vec(sentences=sentences, vector_size=vector_size,
+                                   window=window, min_count=min_count, workers=workers,
+                                   seed=seed, negative=negative, sg=cbow)
+    model.train(sentences, total_examples=model.corpus_count, epochs=epochs)
     return model
