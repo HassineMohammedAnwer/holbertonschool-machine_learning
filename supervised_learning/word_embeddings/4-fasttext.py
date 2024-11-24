dfd590d7cbb5ba8d1c43bb9c5b040eb4ff4382ae
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-11-24 22:32:16 +0100

    NLP/03_update_style

---
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""4. FastText"""
+import gensim
+
+
+def fasttext_model(sentences, vector_size=100, min_count=5, negative=5,
+                   window=5, cbow=True, epochs=5, seed=0, workers=1):
+    """creates , builds and trains a gensim word2vec model:
+    sentences is a list of sentences to be trained on
+    vector_size is the dimensionality of the embedding layer
+    min_count is the minimum number of occurrences of a word for use
+    __in training
+    window is the maximum distance between the current and predicted
+    __word within a sentence
+    negative is the size of negative sampling
+    cbow is a boolean to determine the training type; True is for
+    __CBOW; False is for Skip-gram
+    epochs is the number of iterations to train over
+    seed is the seed for the random number generator
+    workers is the number of worker threads to train the model
+    Returns: the trained model"""
+    # skip-gram/CBOW models,use either hier softmax or negative sampling
+    # sg ({0, 1}, optional) – Training algorithm:1for skip-gram;or CBOW
+    sg = 0 if cbow else 1
+    model = gensim.models.Word2Vec(sentences=sentences, negative=negative,
+                                   vector_size=vector_size, window=window,
+                                   min_count=min_count, workers=workers,
+                                   seed=seed, negative=negative, sg=sg)
+    model.build_vocab(sentences)
+    # prepare the model vocabulary
+    model.train(sentences, total_examples=model.corpus_count, epochs=epochs)
+    return model
