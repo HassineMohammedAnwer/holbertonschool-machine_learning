d628e40644b9ce26562795c1429fc09472ec5ded
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-11-24 22:47:53 +0100

    NLP/04_update

--- supervised_learning/word_embeddings/4-fasttext.py
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -5,16 +5,16 @@ import gensim
 
 def fasttext_model(sentences, vector_size=100, min_count=5, negative=5,
                    window=5, cbow=True, epochs=5, seed=0, workers=1):
-    """creates , builds and trains a gensim word2vec model:
+    """creates, builds and trains a genism fastText model:
     sentences is a list of sentences to be trained on
     vector_size is the dimensionality of the embedding layer
     min_count is the minimum number of occurrences of a word for use
     __in training
-    window is the maximum distance between the current and predicted
-    __word within a sentence
+    window is the maximum distance between the current and predicted word
+    __within a sentence
     negative is the size of negative sampling
-    cbow is a boolean to determine the training type; True is for
-    __CBOW; False is for Skip-gram
+    cbow is a boolean to determine the training type; True is for CBOW;
+    __False is for Skip-gram
     epochs is the number of iterations to train over
     seed is the seed for the random number generator
     workers is the number of worker threads to train the model
@@ -22,9 +22,11 @@ def fasttext_model(sentences, vector_size=100, min_count=5, negative=5,
     # skip-gram/CBOW models,use either hier softmax or negative sampling
     # sg ({0, 1}, optional) – Training algorithm:1for skip-gram;or CBOW
     sg = 0 if cbow else 1
-    model = gensim.models.Word2Vec(sentences=sentences,
+    model = gensim.models.FastText(sentences=sentences,
                                    vector_size=vector_size, window=window,
                                    min_count=min_count, workers=workers,
                                    seed=seed, negative=negative, sg=sg)
-    model.train(sentences, total_examples=len(sentences), epochs=epochs)
+    model.build_vocab(sentences)
+    # prepare the model vocabulary
+    model.train(sentences, total_examples=model.corpus_count, epochs=epochs)
     return model
