78841d3f006dbe5b44863886993ab32c24acd31b
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-11-27 18:58:53 +0100

    NLP_metrics/01_update

--- supervised_learning/nlp_metrics/1-ngram_bleu.py
+++ supervised_learning/nlp_metrics/1-ngram_bleu.py
@@ -5,11 +5,10 @@ import numpy as np
 
 
 def generate_ngram(sentence, order):
-    sentence_ngrams = [' '.join(sentence[i:
-                                         i + order])
-                                         for i in range(len(sentence)
-                                                        - order + 1)]
+    sentence_ngrams = [' '.join(
+        sentence[i:i + order])for i in range(len(sentence) - order + 1)]
     return sentence_ngrams
+
 
 def ngram_bleu(references, sentence, n):
     """calculates the n-gram BLEU score for a sentence:
@@ -19,17 +18,16 @@ def ngram_bleu(references, sentence, n):
     n is the size of the n-gram to use for evaluation
     Returns: the n-gram BLEU score"""
     sentence_ngrams = generate_ngram(sentence, n)
-    print(sentence_ngrams)
-    sentence_counts = Counter(sentence_ngrams)
+    s_counts = Counter(sentence_ngrams)
     max_ref_counts = {}
     for reference in references:
         reference_ngrams = generate_ngram(reference, n)
         reference_counts = Counter(reference_ngrams)
-        for ngram in sentence_counts:
+        for ngram in s_counts:
             max_ref_counts[ngram] = max(max_ref_counts.get(ngram, 0),
                                         reference_counts.get(ngram, 0))
-    count_clip = sum(min(sentence_counts[ngram],
-                         max_ref_counts.get(ngram, 0)) for ngram in sentence_counts)
+    count_clip = sum(min(s_counts[ngram],
+                         max_ref_counts.get(ngram, 0)) for ngram in s_counts)
     precision = count_clip / len(sentence_ngrams)
     sentence_length = len(sentence)
     closest_ref_len = min((abs(len(ref) - sentence_length), len(ref))
