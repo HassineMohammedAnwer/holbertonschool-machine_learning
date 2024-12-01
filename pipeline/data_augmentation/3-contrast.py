058761968c12a34671719b8f055737f122925eca
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-12-01 19:07:43 +0100

    pipeline/data_augmentation/03_update

--- pipeline/data_augmentation/3-contrast.py
+++ pipeline/data_augmentation/3-contrast.py
@@ -7,12 +7,12 @@ import tensorflow as tf
 def change_contrast(image, lower, upper):
     """randomly adjusts the contrast of an image.
     image: A 3D tf.Tensor representing the input image to adjust the contrast.
-    lower: A float representing the lower bound of the random contrast factor range.
-    upper: A float representing the upper bound of the random contrast factor range.
+    lower: float representing lower bound of random contrast factor range.
+    upper: float representing upper bound of random contrast factor range.
     Returns the contrast-adjusted image."""
     image = tf.image.convert_image_dtype(image, tf.float32)
     seed = (0, 0)
-    contrast_image =  tf.image.stateless_random_contrast(
+    contrast_image = tf.image.stateless_random_contrast(
         image=image, lower=lower, upper=upper, seed=seed
     )
-    return contrast_image
+    return tf.image.convert_image_dtype(contrast_image, image.dtype)
