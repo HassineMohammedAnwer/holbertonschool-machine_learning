cc0059e40254126674bd40b27bb78fb846100acb
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-12-01 19:57:40 +0100

    pipeline/data_augmentation/03_update

--- pipeline/data_augmentation/3-contrast.py
+++ pipeline/data_augmentation/3-contrast.py
@@ -5,13 +5,8 @@ import tensorflow as tf
 
 
 def change_contrast(image, lower, upper):
-    """randomly adjusts the contrast of an image.
-    image: A 3D tf.Tensor representing the input image to adjust the contrast.
-    lower: float representing lower bound of random contrast factor range.
-    upper: float representing upper bound of random contrast factor range.
-    Returns the contrast-adjusted image."""
-    image = tf.image.convert_image_dtype(image, tf.float32)
-    contrast_image = tf.image.stateless_random_contrast(
-        image=image, lower=lower, upper=upper
-    )
-    return tf.image.convert_image_dtype(contrast_image, image.dtype)
+    """ rotates an image by 90 degrees counter-clockwise:
+    image is a 3D tf.Tensor containing the image to rotate
+    Returns the rotated image"""
+    return tf.image.random_contrast(
+        image, lower, upper)
