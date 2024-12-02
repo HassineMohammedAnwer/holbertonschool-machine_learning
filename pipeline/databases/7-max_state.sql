a1c6919b36cd2e67706b783443b7c75d54413e1a
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-12-02 19:32:36 +0100

    pipeline/databases/07_update

--- pipeline/databases/7-max_state.sql
+++ pipeline/databases/7-max_state.sql
@@ -1,5 +1,5 @@
 -- Write a script displays the max temperature of each state (ordered by State name).
-SELECT state, avg(value) as max_temp
+SELECT state, max(value) as max_temp
 FROM temperatures
 GROUP BY state
 ORDER BY state ASC
