3e1a9c6cffde2f5be9f3b6dea5345d4b203aca31
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-12-03 16:44:21 +0100

    pipeline/databases/12_update

--- pipeline/databases/12-rating_genres.sql
+++ pipeline/databases/12-rating_genres.sql
@@ -6,8 +6,8 @@
 SELECT tv_genres.name , SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
 JOIN tv_show_genres
-on tv_show_genres.show_id = tv_shows.id
+on tv_show_genres.genre_id = tv_genres.id
 JOIN tv_show_ratings
-on tv_show_ratings.show_id = tv_shows.id
+on tv_show_ratings.show_id = tv_show_genres.show_id
 GROUP BY tv_genres.name
 ORDER BY rating DESC;
