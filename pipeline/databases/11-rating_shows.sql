8df53d4717c12b51cc4676b50f2ac1e6d7885f30
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-12-03 01:21:29 +0100

    pipeline/databases/11

--- pipeline/databases/11-rating_shows.sql
+++ pipeline/databases/11-rating_shows.sql
@@ -5,7 +5,7 @@
 -- The database name will be passed as an argument of the mysql command
 SELECT tv_shows.title , SUM(tv_show_genres.rate)
 FROM tv_show
-LEFT JOIN tv_show_ratings
+LEFT JOIN tv_show_ratings tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 GROUP BY tv_shows.title
 ORDER BY SUM(tv_show_genres.rate) DESC
