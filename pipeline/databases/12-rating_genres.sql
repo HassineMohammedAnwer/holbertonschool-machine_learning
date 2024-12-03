a3cf89b64917ebbf8f5fd5f96cb6126f9e7cfa8b
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-12-03 16:39:57 +0100

    pipeline/databases/12_update

--- pipeline/databases/12-rating_genres.sql
+++ pipeline/databases/12-rating_genres.sql
@@ -3,11 +3,11 @@
 -- Results must be sorted in descending order by their rating
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
-SELECT tv_genres , SUM(tv_show_ratings.rate) AS rating
+SELECT tv_genres.name , SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows
 JOIN tv_show_genres
-on tv_show_genres.genre_id = tv_genres.id
+on tv_show_genres.show_id = tv_shows.id
 JOIN tv_show_ratings
-ON tv_show_ratings.show_id = tv_show_genres.show_id
+on tv_show_ratings.show_id = tv_shows.id
 GROUP BY tv_genres.name
 ORDER BY rating DESC;
