0e3e59b684e8a8497697ec91bbcc076500ff62cb
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-12-03 15:43:43 +0100

    pipeline/databases/11_update

--- pipeline/databases/11-rating_shows.sql
+++ pipeline/databases/11-rating_shows.sql
@@ -6,6 +6,6 @@
 SELECT tv_shows.title , SUM(tv_show_ratings.rate)
 FROM tv_show_ratings
 JOIN tv_shows
-ON tv_show_ratings.show_id = tv_show.id
+ON tv_show_ratings.show_id = tv_shows.id
 GROUP BY tv_shows.title
 ORDER BY rating DESC
