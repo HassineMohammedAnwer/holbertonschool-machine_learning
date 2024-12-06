f945a1d905d76e6c7d19277b0cb331d15aaf6a6a
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-12-06 21:33:43 +0100

    clustering/06

--- unsupervised_learning/clustering/6-expectation.py
+++ unsupervised_learning/clustering/6-expectation.py
@@ -10,7 +10,25 @@ def expectation(X, pi, m, S):
     pi is a numpy.ndarray of shape (k,) containing the priors for each cluster
     m is a numpy.ndarray of shape (k, d) containing the centroid means for each cluster
     S is a numpy.ndarray of shape (k, d, d) containing the covariance matrices for each cluster
+    You may use at most 1 loop
+    Returns: g, l, or None, None on failure
+    g is a numpy.ndarray of shape (k, n) containing the posterior probabilities for each data point in each cluster
+    l is the total log likelihood
     """
-    k = pi.shape[0]
-    pdfs = np.array([pdf(X, m[i], S[i]) for i in range(k)])
+    try:
+        n, d = X.shape
+        k = pi.shape[0]
+        g = np.zeros((k, n))
+
+        # (pi_k * P_k(X)) / sum((pi_k * P_k(X)))
+        for j in range(k):
+            P_k = pdf(X, m[j], S[j])
+            g[j] = pi[j] * P_k
+        d = np.sum(g, axis=0)
+        g /= d
+        # l = sum(log( sum((pi_k * P_k(X)) ))
+        l = np.sum(np.log(d))
+        return g, l
+    except Exception:
+        return None, None
     
