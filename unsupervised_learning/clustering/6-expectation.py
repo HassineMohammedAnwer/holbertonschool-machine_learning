8768ed318a2a9b8994a392d76af3ea4c90684472
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-12-06 21:40:23 +0100

    clustering/06_update

--- unsupervised_learning/clustering/6-expectation.py
+++ unsupervised_learning/clustering/6-expectation.py
@@ -8,11 +8,14 @@ def expectation(X, pi, m, S):
     """calculates the expectation step in the EM algorithm for a GMM
     X is a numpy.ndarray of shape (n, d) containing the data set
     pi is a numpy.ndarray of shape (k,) containing the priors for each cluster
-    m is a numpy.ndarray of shape (k, d) containing the centroid means for each cluster
-    S is a numpy.ndarray of shape (k, d, d) containing the covariance matrices for each cluster
+    m is a numpy.ndarray of shape (k, d) containing
+    __the centroid means for each cluster
+    S is a numpy.ndarray of shape (k, d, d) containing the
+    __covariance matrices for each cluster
     You may use at most 1 loop
     Returns: g, l, or None, None on failure
-    g is a numpy.ndarray of shape (k, n) containing the posterior probabilities for each data point in each cluster
+    g is a numpy.ndarray of shape (k, n) containing the posterior
+    __probabilities for each data point in each cluster
     l is the total log likelihood
     """
     try:
