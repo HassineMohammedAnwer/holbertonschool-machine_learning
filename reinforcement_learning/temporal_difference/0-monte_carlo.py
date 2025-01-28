a97d48948a8b8e59c7976c6665d05073024e15e4
HassineMohammedAnwer <3677@holbertonstudents.com>
2025-01-28 13:40:54 +0100

    rl/temporal_diff_update

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -9,7 +9,7 @@ def monte_carlo(env, V, policy, episodes=5000,
     Performs the Monte Carlo algorithm:
     env is environment instance
     V is a numpy.ndarray of shape (s,) containing the value estimate
-    policy is a function that takes in a state and returns the next action to take
+    policy:function: takes in a state and returns the next action to take
     episodes is the total number of episodes to train over
     max_steps is the maximum number of steps per episode
     alpha is the learning rate
