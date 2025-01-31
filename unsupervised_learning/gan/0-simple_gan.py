9602088f74d61bcac6f5a0f34356cf2713ef9986
HassineMohammedAnwer <3677@holbertonstudents.com>
2025-01-31 21:11:58 +0100

    Update 0-simple_gan.py

--- unsupervised_learning/gan/0-simple_gan.py
+++ unsupervised_learning/gan/0-simple_gan.py
@@ -31,24 +31,7 @@ class Simple_GAN(keras.Model):
         # define the discriminator loss and optimizer:
         self.discriminator.loss      = lambda x,y : tf.keras.losses.MeanSquaredError()(x, tf.ones(x.shape)) + tf.keras.losses.MeanSquaredError()(y, -1*tf.ones(y.shape))
         self.discriminator.optimizer = keras.optimizers.Adam(learning_rate=self.learning_rate, beta_1=self.beta_1, beta_2=self.beta_2)
-        self.discriminator.compile(optimizer=discriminator.optimizer , loss=discriminator.loss )
-
-    # generator of fake samples of size batch_size
-    def get_fake_sample(self, training=False):
-        """A fake sample is just the image of the generator applied to a latent sample"""
-        if not size :
-            size= self.batch_size
-        return self.generator(self.latent_generator(size), training=training)
-
-    # generator of real samples of size batch_size
-    def get_real_sample(self):
-        """A real sample is a random subset of the set of real_examples"""
-        if not size :
-            size= self.batch_size
-        sorted_indices = tf.range(tf.shape(self.real_examples)[0])
-        random_indices  = tf.random.shuffle(sorted_indices)[:size]
-        return tf.gather(self.real_examples, random_indices)
-             
+        self.discriminator.compile(optimizer=discriminator.optimizer , loss=discriminator.loss )             
     # overloading train_step()
     def train_step(self,useless_argument): 
         """ GANs train"""
