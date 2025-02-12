41de61005d71e1c1a2e7b23e4d832548030a3c99
HassineMohammedAnwer <3677@holbertonstudents.com>
2025-02-12 11:57:14 +0100

    tr_app

--- supervised_learning/transformer_apps/3-dataset.py
+++ supervised_learning/transformer_apps/3-dataset.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 
 class Dataset:
     """class dataset"""
-    def __init__(self, batch_size, max_len):
+    def __init__(self):
         """Class constructor"""
         self.data_train, self.data_valid = tfds.load(
             'ted_hrlr_translate/pt_to_en',
@@ -17,7 +17,8 @@ class Dataset:
 
         self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
             self.data_train)
-
+        self.data_train = self.data_train.map(self.tf_encode)
+        self.data_valid = self.data_valid.map(self.tf_encode)
 
     def tokenize_dataset(self, data):
         """tokenize"""
@@ -44,19 +45,30 @@ class Dataset:
 
     def encode(self, pt, en):
         """encode"""
-        pt_tokens = self.tokenizer_pt.encode(pt.numpy().decode('utf-8'), add_special_tokens=True)
-        en_tokens = self.tokenizer_en.encode(en.numpy().decode('utf-8'), add_special_tokens=True)
-        
-        pt_tokens = self.tokenizer_pt.encode(pt_tokens,
-                                             add_special_tokens=False)
-        en_tokens = self.tokenizer_en.encode(en_tokens,
-                                             add_special_tokens=False)
-        
-        return pt_tokens, en_tokens
+        pt_text = pt.numpy().decode('utf-8')
+        en_text = en.numpy().decode('utf-8')
+        pt_start = len(self.tokenizer_pt)
+        pt_end = len(self.tokenizer_pt) + 1
+        en_start = len(self.tokenizer_en)
+        en_end = len(self.tokenizer_en) + 1
+
+        pt_token_ids = self.tokenizer_pt.encode(pt_text,
+                                                add_special_tokens=False)
+        en_token_ids = self.tokenizer_en.encode(en_text,
+                                                add_special_tokens=False)
+
+        encoded_pt = [pt_start] + pt_token_ids + [pt_end]
+        encoded_en = [en_start] + en_token_ids + [en_end]
+
+        return encoded_pt, encoded_en
 
     def tf_encode(self, pt, en):
         """tf_encode"""
-        pt_encoded, en_encoded = tf.py_function(self.encode, [pt, en], [tf.int64, tf.int64])
+        pt_encoded, en_encoded = tf.py_function(
+            func=self.encode,
+            inp=[pt, en],
+            Tout=[tf.int64, tf.int64]
+        )
         pt_encoded.set_shape([None])
         en_encoded.set_shape([None])
         return pt_encoded, en_encoded
