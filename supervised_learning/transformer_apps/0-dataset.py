0d5c2aa909ff8c7dcaaadd2c3f79db03e3997a22
HassineMohammedAnwer <3677@holbertonstudents.com>
2025-02-12 10:06:30 +0100

    tr_app

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -17,10 +17,21 @@ class Dataset:
 
     def tokenize_dataset(self, data):
         """tokenize"""
-        """tokenizer_pt = transformers.BertTokenizerFast.from_pretrained('neuralmind/bert-base-portuguese-cased')
-        tokenizer_en = transformers.BertTokenizerFast.from_pretrained('bert-base-uncased')
-        
-        tokenizer_pt.model_max_length = 2**13
-        tokenizer_en.model_max_length = 2**13
-        
-        return tokenizer_pt, tokenizer_en"""
+        all_sentences = [(pt.numpy().decode("utf-8"), en.numpy().decode("utf-8"))
+                         for pt, en in data]
+        pt_texts, en_texts = (list(pair) for pair in zip(*all_sentences)) if all_sentences else ([], [])
+
+        base_pt = transformers.AutoTokenizer.from_pretrained(
+            'neuralmind/bert-base-portuguese-cased',
+            use_fast=True,
+            clean_up_tokenization_spaces=True
+        )
+        base_en = transformers.AutoTokenizer.from_pretrained(
+            'bert-base-uncased',
+            use_fast=True,
+            clean_up_tokenization_spaces=True
+        )
+
+        new_pt = base_pt.train_new_from_iterator(pt_texts, vocab_size=2**13)
+        new_en = base_en.train_new_from_iterator(en_texts, vocab_size=2**13)
+        return new_pt, new_en
