20f7c1621342811e58f168d6d6e103cc125f2020
HassineMohammedAnwer <3677@holbertonstudents.com>
2025-04-05 22:29:17 +0000

    databases/20

---
+++ pipeline/databases/20-average_score.sql
@@ -0,0 +1,19 @@
+-- creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student.
+-- Requirements:
+-- Procedure ComputeAverageScoreForUser is taking 1 input:
+-- user_id, a users.id value (you can assume user_id is linked to an existing users)
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+CREATE PROCEDURE ComputeAverageScoreForUser (
+    IN user_id INT
+)
+BEGIN
+    DECLARE calculated_average FLOAT;
+    SELECT COALESCE(AVG(score), 0)
+    INTO calculated_average
+    FROM corrections
+    WHERE corrections.user_id = user_id;
+    UPDATE users
+    SET average_score = calculated_average
+    WHERE id = user_id;
+END$$
+DELIMITER ;
