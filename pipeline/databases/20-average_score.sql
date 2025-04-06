dd02c95ac369b9d40472c31e15cf781c5e916c96
HassineMohammedAnwer <3677@holbertonstudents.com>
2025-04-06 15:51:40 +0000

    oj

--- pipeline/databases/20-average_score.sql
+++ pipeline/databases/20-average_score.sql
@@ -2,6 +2,8 @@
 -- Requirements:
 -- Procedure ComputeAverageScoreForUser is taking 1 input:
 -- user_id, a users.id value (you can assume user_id is linked to an existing users)
+DELIMITER //
+
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 CREATE PROCEDURE ComputeAverageScoreForUser (
     IN user_id INT
