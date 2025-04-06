40b3e659b63b1d468e952803ad432fcf3d1b4685
HassineMohammedAnwer <3677@holbertonstudents.com>
2025-04-06 15:54:48 +0000

    oj

--- pipeline/databases/20-average_score.sql
+++ pipeline/databases/20-average_score.sql
@@ -9,8 +9,6 @@ CREATE PROCEDURE ComputeAverageScoreForUser (
     IN user_id INT
 )
 BEGIN
-	UPDATE users SET average_score=(
-	SELECT AVG(score) FROM corrections WHERE user_id=user_id_new)
-	WHERE id=user_id_new;
+	UPDATE users 
 END; //
 DELIMITER ;
