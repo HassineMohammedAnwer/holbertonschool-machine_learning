19fe652c9d1ce612b09ae9eaa010244a3d4f2a89
HassineMohammedAnwer <3677@holbertonstudents.com>
2025-04-11 10:30:49 +0000

    lkjh

--- supervised_learning/attention/10-transformer_decoder.py
+++ supervised_learning/attention/10-transformer_decoder.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""
+transformer"""
+
 import tensorflow as tf
 positional_encoding = __import__('4-positional_encoding').positional_encoding
 DecoderBlock = __import__('8-transformer_decoder_block').DecoderBlock
