e02fa08697efe6cb836bb7af0fe75dc03f41df30
HassineMohammedAnwer <3677@holbertonstudents.com>
2025-04-11 10:23:09 +0000

    lkjh

--- supervised_learning/attention/10-transformer_decoder.py
+++ supervised_learning/attention/10-transformer_decoder.py
@@ -3,10 +3,12 @@ import tensorflow as tf
 positional_encoding = __import__('4-positional_encoding').positional_encoding
 DecoderBlock = __import__('8-transformer_decoder_block').DecoderBlock
 
+
 class Decoder(tf.keras.layers.Layer):
     """
     Decoder"""
-    def __init__(self, N, dm, h, hidden, target_vocab, max_seq_len, drop_rate=0.1):
+    def __init__(self, N, dm, h, hidden, target_vocab, max_seq_len,
+    drop_rate=0.1):
         """
         N: Number of blocks in the decoder
         dm: Dimensionality of the model
@@ -24,7 +26,7 @@ class Decoder(tf.keras.layers.Layer):
         self.blocks = [DecoderBlock(dm, h, hidden, drop_rate) 
                       for _ in range(N)]
         self.dropout = tf.keras.layers.Dropout(drop_rate)
-    
+
     def call(self, x, encoder_output, training, look_ahead_mask, padding_mask):
         """
         x - a tensor of shape (batch, target_seq_len, dm)
