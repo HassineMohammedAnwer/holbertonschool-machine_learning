4759899d1abac469ca95ac6eb326cf536c89ae07
HassineMohammedAnwer <3677@holbertonstudents.com>
2025-04-11 10:25:35 +0000

    lkjh

--- supervised_learning/attention/10-transformer_decoder.py
+++ supervised_learning/attention/10-transformer_decoder.py
@@ -8,7 +8,7 @@ class Decoder(tf.keras.layers.Layer):
     """
     Decoder"""
     def __init__(self, N, dm, h, hidden, target_vocab, max_seq_len,
-    drop_rate=0.1):
+                 drop_rate=0.1):
         """
         N: Number of blocks in the decoder
         dm: Dimensionality of the model
@@ -23,8 +23,8 @@ class Decoder(tf.keras.layers.Layer):
         self.dm = dm
         self.embedding = tf.keras.layers.Embedding(target_vocab, dm)
         self.positional_encoding = positional_encoding(max_seq_len, dm)
-        self.blocks = [DecoderBlock(dm, h, hidden, drop_rate) 
-                      for _ in range(N)]
+        self.blocks = [DecoderBlock(dm, h, hidden, drop_rate)
+            for _ in range(N)]
         self.dropout = tf.keras.layers.Dropout(drop_rate)
 
     def call(self, x, encoder_output, training, look_ahead_mask, padding_mask):
@@ -34,8 +34,10 @@ class Decoder(tf.keras.layers.Layer):
         encoder_output - a tensor of shape
         __(batch, input_seq_len, dm)containing the output of the encoder
         training - a boolean to determine if the model is training
-        look_ahead_mask - the mask to be applied to the first multi head attention layer
-        padding_mask - the mask to be applied to the second multi head attention layer
+        look_ahead_mask - the mask to be applied to
+        __the first multi head attention layer
+        padding_mask - the mask to be applied to
+        __the second multi head attention layer
         Returns: a tensor of shape (batch, target_seq_len, dm)
         __containing the decoder output
         """
@@ -45,6 +47,6 @@ class Decoder(tf.keras.layers.Layer):
         x += self.positional_encoding[:seq_len, :]
         x = self.dropout(x, training=training)
         for i in range(self.N):
-            x = self.blocks[i](x, encoder_output, training, 
-                             look_ahead_mask, padding_mask)
+            x = self.blocks[i](x, encoder_output, training,
+                               look_ahead_mask, padding_mask)
         return x
