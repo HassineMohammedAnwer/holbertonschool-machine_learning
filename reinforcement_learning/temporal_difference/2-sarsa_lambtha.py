24543e9a4dde0eb9ce6d25fce3bcb6f4413aeebe
HassineMohammedAnwer <3677@holbertonstudents.com>
2025-04-11 03:40:53 +0000

    sarsa_lambdha

--- reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
+++ reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
@@ -20,25 +20,26 @@ def sarsa_lambtha(env, Q, lambtha, episodes=5000, max_steps=100, alpha=0.1,
     E = np.zeros_like(Q)
     for episode in range(episodes):
         E.fill(0)
-        done = False
-        truncated = False
         state = env.reset()[0]
         if np.random.random() < epsilon:
-            action = env.action_space.sample()
+            action = np.random.randint(n_actions)
         else:
             action = np.argmax(Q[state])
-        for _ in range(max_steps):
+        step = 0
+        done = False
+        truncated = False
+
+        while not (done or truncated) and step < max_steps:
             next_state, reward, done, truncated, _ = env.step(action)
             if np.random.random() < epsilon:
-                next_action = env.action_space.sample()
+                next_action = np.random.randint(n_actions)
             else:
                 next_action = np.argmax(Q[next_state])
-            td_error = reward + gamma * Q[next_state][next_action] - Q[state][action]
-            E[state][action] += 1
+            td_error = reward + gamma * Q[next_state, next_action] - Q[state, action]
+            E[state, action] += 1
             Q += alpha * td_error * E
             E *= gamma * lambtha
             state, action = next_state, next_action
-            if done or truncated:
-                break
+            step += 1
         epsilon = max(min_epsilon, epsilon * (1 - epsilon_decay))
     return Q
