9faaebd3fafd30d2c5df30c2da56b689b80907c5
HassineMohammedAnwer <3677@holbertonstudents.com>
2025-04-15 22:56:26 +0000

    lkj

--- unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
@@ -9,6 +9,7 @@ class BayesianOptimization:
     """Performs Bayesian optimization on a noiseless 1D Gaussian process"""
     def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1,
                  sigma_f=1, xsi=0.01, minimize=True):
+        """initialize"""
         self.f = f
         self.X_s = np.linspace(bounds[0], bounds[1], ac_samples).reshape(-1, 1)
         self.xsi = xsi
@@ -40,4 +41,17 @@ class BayesianOptimization:
         """
         Optimizes the black-box function
         """
-        return [1]
+        for _ in range(iterations):
+            X_next, _ = self.acquisition()
+            if any(np.allclose(X_next, x_prev) for x_prev in self.gp.X):
+                break
+            Y_next = self.f(X_next)
+            self.gp.update(X_next, Y_next)
+        if self.minimize:
+            idx = np.argmin(self.gp.Y)
+        else:
+            idx = np.argmax(self.gp.Y)
+        X_opt = self.gp.X[idx]
+        Y_opt = self.gp.Y[idx]
+
+        return X_opt, Y_opt
