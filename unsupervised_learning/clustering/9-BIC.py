1f188c7979c9ec7ebe2ed1cbb37067e007cd0e2e
HassineMohammedAnwer <3677@holbertonstudents.com>
2025-04-16 13:35:34 +0000

    BIC

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -6,37 +6,37 @@ expectation_maximization = __import__('8-EM').expectation_maximization
 
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
-    """finds the best number of clusters for a
+    """finds the best number of clusters  a
     __GMM using the Bayesian Information Criterion:
     X is a numpy.ndarray of shape (n, d) containing the data set
     kmin is a positive integer containing the minimum
-    __number of clusters to check for (inclusive)
+    __number of clusters to check (inclusive)
     kmax is a positive integer containing the maximum
-    __number of clusters to check for (inclusive)
+    __number of clusters to check (inclusive)
     If kmax is None, kmax should be set to the maximum
     __number of clusters possible
     iterations is a positive integer containing the
-    __maximum number of iterations for the EM algorithm
+    __maximum number of iterations the EM algorithm
     tol is a non-negative float containing the tolerance
-    __for the EM algorithm
+    __ the EM algorithm
     verbose is a boolean that determines if the EM algorithm
     __should print information to the standard output
     Returns: best_k, best_result, l, b, or
     __None, None, None, None on failure
-    best_k is the best value for k based on its BIC
+    best_k is the best value  k based on its BIC
     best_result is tuple containing pi, m, S
     pi is a numpy.ndarray of shape (k,) containing the
-    __cluster priors for the best number of clusters
+    __cluster priors  the best number of clusters
     m is a numpy.ndarray of shape (k, d) containing the
-    __centroid means for the best number of clusters
+    __centroid means  the best number of clusters
     S is a numpy.ndarray of shape (k, d, d) containing the
-    __covariance matrices for the best number of clusters
+    __covariance matrices  the best number of clusters
     l is a numpy.ndarray of shape (kmax - kmin + 1) containing
-    __the log likelihood for each cluster size tested
+    __the log likelihood  each cluster size tested
     b is a numpy.ndarray of shape (kmax - kmin + 1) containing
-    __the BIC value for each cluster size tested
+    __the BIC value  each cluster size tested
     Use: BIC = p * ln(n) - 2 * l
-    p is the number of parameters required for the model
+    p is the number of parameters required  the model
     n is the number of data points used to create the model
     l is the log likelihood of the model"""
     # Input validation
@@ -56,11 +56,11 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
     if kmax is None:
         kmax = n
 
-    # additional validation for kmax
+    # additional validation kmax
     if kmax < kmin or kmax > n:
         return None, None, None, None
 
-    # check if kmin is too large for the dataset
+    # check if kmin is too large the dataset
     if kmin > n:
         return None, None, None, None
 
@@ -92,9 +92,9 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
             return None, None, None, None
 
         # calculate number of parameters
-        # k-1 for pi (priors sum to 1)
-        # k*d for means
-        # k*d*(d+1)/2 for covariance matrices (symmetric)
+        # k-1 pi (priors sum to 1)
+        # k*d means
+        # k*d*(d+1)/2 covariance matrices (symmetric)
         p = k - 1 + k * d + k * d * (d + 1) / 2
 
         # calculate BIC
